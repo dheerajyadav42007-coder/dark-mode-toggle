78844f24f41e5820584faf5d56e2145fee105407
dheerajyadav42007-coder <dheerajyadav42007@gmail.com>
2026-01-14 19:15:41 +0530

    Implement background color toggle based on checkbox
    Add functionality to change body background color based on input checkbox state.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const 
+const inputEl = document.querySelector(".input");
+
+const bodyEl=document.querySelector("body");
+
+inputEl.checked=true;
+
+updatebody()
+
+
+
+function updatebody(){
+    if(inputEl.checked){
+        bodyEl.style.background="black"
+    }
+    else{
+        bodyEl.style.background="white"
+    }
+}
+
+inputEl.addEventListener("input", ()=>{
+    updatebody()
+})
